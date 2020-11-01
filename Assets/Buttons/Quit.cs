d68dc7f1d129cdbaae9f0ce84453d50a291daba5
roden1978 <rdn.homebox@gmail.com>
2020-11-01 12:50:47 +0500

    Добавил кнопку выхода, мелкие доработки

---
+++ Assets/Buttons/Quit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Quit : MonoBehaviour, IPointerDownHandler
+{
+   
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		if (gameObject.name == "QuitButton")
+		{
+			ApplicationQuit();
+		}
+	}
+    
+	public void ApplicationQuit () {
+		Application.Quit();
+		
+		if (Application.platform == RuntimePlatform.Android) {
+			AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+			activity.Call("finishAndRemoveTask");
+		}
+		
+		Debug.Log("Quit");
+	}
+}
