8c9dc69e510fd5d60cd310fae319a660f409eeaa
roden1978 <rdn.homebox@gmail.com>
2020-12-15 13:51:33 +0500

    Продолжение разработки визуальных эффектов выстрелов, остались Alligator, Agressor, Widowmaker

--- Assets/ScriptableObjects/Weapon/Firearms/Shoots/BloodthornShoot.cs
+++ Assets/ScriptableObjects/Weapon/Firearms/Shoots/BloodthornShoot.cs
@@ -25,15 +25,11 @@ public class BloodthornShoot : AWeaponShoot
 		return bulletGameObject;
 	}
 
-	private GameObject CreateFlame(Vector3 _shootPoint, Quaternion _rotation)
+	private void CreateFlame(Vector3 _shootPoint, Quaternion _rotation)
 	{
 		if (weapon && bullet) {
-        			//Instantiate(weapon.VFXShoot, shootPoint, weapon.ShootPointRotation);
-                    bulletGameObject =  Instantiate(bullet, _shootPoint, _rotation);
-        }
-        
-        Debug.Log("Оружие или пуля не найдены скрипт BloodthornShoot");
-        
-        return null;
+        	bulletGameObject =  Instantiate(bullet, _shootPoint, _rotation);
+        }else Debug.Log("Оружие или пуля не найдены скрипт BloodthornShoot");
 	}
+	
 }
